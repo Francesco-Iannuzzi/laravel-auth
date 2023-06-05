f565be7a7acbd231c4ed5bbf0844d690ac49f79e
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-05 16:53:42 +0200

    add fillable in models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'made_by', 'description', 'creation_date'];
+
+    public static function generateSlug($title)
+    {
+        return Str::slug($title, '-');
+    }
 }
